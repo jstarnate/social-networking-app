2e2f661dd05c28d0ceeceb21d5240a2c0c22f7aa
Jonathan Tarnate <jstarnate@gmail.com>
2020-11-21 17:51:22 +0800

    Removed close button on the header and changed some class names.

--- resources/ts/components/helpers/Modal.tsx
+++ resources/ts/components/helpers/Modal.tsx
@@ -4,7 +4,6 @@ interface ModalProps {
     title: string;
     type: string;
     message: string | null;
-    closeEvent?: () => void;
     children?: ReactElement;
 }
 
@@ -12,20 +11,17 @@ const AlertBox: FC<ModalProps> = ({
     title,
     type,
     message,
-    closeEvent,
     children,
 }: ModalProps): ReactElement => {
     return (
-        <div className='pos--fixed modal'>
-            <div className='bg--white b-rad--md mg-l--auto mg-r--auto modal__main'>
-                <header className={`d--flex bg--${type} pd--sm modal__head`}>
+        <div className='pos--fixed home__modal'>
+            <div className='bg--white b-rad--md mg-l--auto mg-r--auto home__modal-main'>
+                <header
+                    className={`d--flex bg--${type} pd--xs home__modal-head`}>
                     <h3 className='text--white'>{title}</h3>
-                    <button className='btn mg-l--auto' onClick={closeEvent}>
-                        <i className='fa fa-times font--lg text--white'></i>
-                    </button>
                 </header>
 
-                <p className='font--md text--black-light pd--sm modal__body'>
+                <p className='font--md text--black-light pd-t--sm pd-b--sm pd-l--xs pd-r--xs home__modal-body'>
                     {message}
                 </p>
 
