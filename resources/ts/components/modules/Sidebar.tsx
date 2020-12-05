b668e5ff30111817dcfb000b538dc5f06f8f2d5b
Jonathan Tarnate <jstarnate@gmail.com>
2020-12-05 10:03:17 +0800

    Added notifCount and updateNotifStatus props.

--- resources/ts/components/modules/Sidebar.tsx
+++ resources/ts/components/modules/Sidebar.tsx
@@ -1,14 +1,28 @@
-import React, { FC, ReactElement } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React, { FC, ReactElement, useEffect } from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import MaleDefaultAvatar from 'helpers/MaleDefaultAvatar';
 import FemaleDefaultAvatar from 'helpers/FemaleDefaultAvatar';
 import { UserWithId } from 'types/models';
 
 interface SidebarProps {
     user: UserWithId | null;
+    notifCount: number;
+    updateNotifStatus: () => void;
 }
 
-const Sidebar: FC<SidebarProps> = ({ user }: SidebarProps): ReactElement => {
+const Sidebar: FC<SidebarProps> = ({
+    user,
+    notifCount,
+    updateNotifStatus,
+}: SidebarProps): ReactElement => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        if (pathname === '/notifications') {
+            updateNotifStatus();
+        }
+    }, [pathname]);
+
     return (
         <aside className='sidebar'>
             <div className='pos--fixed bg--primary-pale br--1 brdr--primary sidebar__wrap'>
@@ -38,13 +52,19 @@ const Sidebar: FC<SidebarProps> = ({ user }: SidebarProps): ReactElement => {
                             <span className='text--bold mg-l--sm'>Home</span>
                         </NavLink>
                         <NavLink
-                            className='d--block font--lg text--gray pd-t--xs pd-b--xs'
+                            className='d--block font--lg text--gray pd-t--xs pd-b--xs pos--rel'
                             to='/notifications'
                             activeClassName='sidebar__active-link'>
                             <i className='fa fa-bell'></i>
                             <span className='text--bold mg-l--sm'>
                                 Notifications
                             </span>
+
+                            {!!notifCount && (
+                                <span className='font--sm text--white bg--danger round pos--abs sidebar__notif-count'>
+                                    {notifCount > 9 ? '9+' : notifCount}
+                                </span>
+                            )}
                         </NavLink>
                         <NavLink
                             className='d--block font--lg text--gray pd-t--xs pd-b--xs'
