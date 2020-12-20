793c6f1d44dfca1f50372bb982c7b7e993da6e19
Jonathan Tarnate <jstarnate@gmail.com>
2020-12-20 16:31:30 +0800

    Added limited characters indicator

--- resources/ts/components/pages/home/PostBox.tsx
+++ resources/ts/components/pages/home/PostBox.tsx
@@ -3,13 +3,23 @@ import { useDispatch } from 'react-redux';
 import axios from 'axios';
 import { unshiftAdd } from 'actions';
 
+const totalChars = 170;
+
 const PostBox: FC = (): ReactElement => {
     const [body, setBody] = useState<string | null>(null);
     const [loading, setLoading] = useState<boolean>(false);
+    const [charsLeft, setCharsLeft] = useState<number>(totalChars);
     const dispatch = useDispatch();
 
     function handleBodyValue(event: ChangeEvent<HTMLTextAreaElement>) {
-        setBody(event.target.value);
+        const { value } = event.target;
+
+        if (value.length === totalChars + 1) {
+            return false;
+        }
+
+        setCharsLeft(totalChars - value.length);
+        setBody(value);
     }
 
     async function submitPost() {
@@ -35,9 +45,15 @@ const PostBox: FC = (): ReactElement => {
                 value={body || ''}
                 onChange={handleBodyValue}></textarea>
 
-            <div className='bt--1 brdr--primary d--flex jc--end'>
+            <div className='bt--1 brdr--primary d--flex ai--center pd-l--md'>
+                <label>
+                    <span className='text--black-light text--bold'>
+                        {charsLeft}{' '}
+                    </span>
+                    <span className='text--black-light'>left</span>
+                </label>
                 <button
-                    className='btn text--primary-dark text--bold pd-t--sm pd-b--sm pd-l--md pd-r--md timeline__post-submit'
+                    className='btn text--primary-dark text--bold pd-t--sm pd-b--sm pd-l--md pd-r--md mg-l--auto timeline__post-submit'
                     disabled={loading}
                     onClick={submitPost}>
                     Submit post
