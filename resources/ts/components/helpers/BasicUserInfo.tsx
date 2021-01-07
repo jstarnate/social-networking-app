cd45e356a46602a98336dd646250edaefe504c6d
Jonathan Tarnate <jstarnate@gmail.com>
2021-01-07 17:33:05 +0800

    Added a condition for linked or not linked component

--- resources/ts/components/helpers/BasicUserInfo.tsx
+++ resources/ts/components/helpers/BasicUserInfo.tsx
@@ -6,40 +6,65 @@ import { User } from 'types/models';
 
 interface BasicUserInfoProps extends User {
     className?: string;
-    imageClassName: string;
+    imageClassName?: string;
     avatarSize?: number;
 }
 
-const BasicUserInfo: FC<BasicUserInfoProps> = ({
-    className,
+interface ContentProps {
+    imageClassName?: string;
+    avatarSize?: number;
+    full_name: string | undefined;
+    username: string | undefined;
+    gender: 'Male' | 'Female' | undefined;
+    image_url: string | undefined;
+}
+
+const Content = ({
     imageClassName,
     avatarSize,
     full_name,
     username,
     gender,
     image_url,
+}: ContentProps) => (
+    <>
+        {gender === 'Male' && !image_url ? (
+            <MaleDefaultAvatar size={avatarSize} />
+        ) : gender === 'Female' && !image_url ? (
+            <FemaleDefaultAvatar size={avatarSize} />
+        ) : (
+            <img
+                className={`round ${imageClassName}`}
+                src={image_url}
+                alt='Profile photo'
+            />
+        )}
+
+        <div className='mg-l--xs'>
+            <span className='d--block font--sm text--black-light text--bold'>
+                {full_name}
+            </span>
+            <span className='font--sm text--gray'>@{username}</span>
+        </div>
+    </>
+);
+
+const BasicUserInfo: FC<BasicUserInfoProps> = ({
+    className,
     url,
+    ...props
 }: BasicUserInfoProps): ReactElement => {
+    if (!url) {
+        return (
+            <div className={className}>
+                <Content {...props} />
+            </div>
+        );
+    }
+
     return (
         <Link className={className} to={url || ''}>
-            {gender === 'Male' && !image_url ? (
-                <MaleDefaultAvatar size={avatarSize} />
-            ) : gender === 'Female' && !image_url ? (
-                <FemaleDefaultAvatar size={avatarSize} />
-            ) : (
-                <img
-                    className={`round ${imageClassName}`}
-                    src={image_url}
-                    alt='Profile photo'
-                />
-            )}
-
-            <div className='mg-l--xs'>
-                <span className='d--block font--sm text--black-light text--bold'>
-                    {full_name}
-                </span>
-                <span className='font--sm text--gray'>@{username}</span>
-            </div>
+            <Content {...props} />
         </Link>
     );
 };
