3662c48688c350828119541785986e8e579e8638
Jonathan Tarnate <jstarnate@gmail.com>
2021-01-07 17:29:04 +0800

    Reusable component for labels on profile headline

---
+++ resources/ts/components/pages/profile/InfoLabel.tsx
@@ -0,0 +1,22 @@
+import React, { FC, ReactElement } from 'react';
+
+interface Props {
+    containerClassName?: string | null;
+    icon: string;
+    label: string | undefined;
+}
+
+const InfoLabel: FC<Props> = ({
+    containerClassName,
+    icon,
+    label,
+}: Props): ReactElement => {
+    return (
+        <label className={`font--md text--gray ${containerClassName || ''}`}>
+            <i className={`fa fa-${icon}`}></i>
+            <span className='mg-l--xxs'>{label}</span>
+        </label>
+    );
+};
+
+export default InfoLabel;
