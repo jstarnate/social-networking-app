33f60829272f4383ee937c470d167d36278a1d17
Jonathan Tarnate <jstarnate@gmail.com>
2021-01-07 17:28:11 +0800

    Modal for viewing data or list of data

---
+++ resources/ts/components/helpers/ViewModal.tsx
@@ -0,0 +1,37 @@
+import React, { FC, ReactElement, useRef } from 'react';
+import useOutsideClick from 'hooks/useOutsideClick';
+import useCloseOnEscape from 'hooks/useCloseOnEscape';
+
+interface ViewModalProps {
+    title: string;
+    closeEvent: () => void;
+    children: ReactElement;
+}
+
+const ViewModal: FC<ViewModalProps> = (props: ViewModalProps): ReactElement => {
+    const modal = useRef<HTMLDivElement>(null);
+
+    useCloseOnEscape(props.closeEvent);
+    useOutsideClick(modal, props.closeEvent);
+
+    return (
+        <div className='pos--fixed modal'>
+            <div
+                ref={modal}
+                className='bg--white b-rad--md mg-l--auto mg-r--auto modal__main'>
+                <header className='d--flex ai--center pd--sm bb--1 brdr--gray-light modal__head'>
+                    <h4 className='text--black-light'>{props.title}</h4>
+                    <button
+                        className='btn text--gray text--bold mg-l--auto'
+                        onClick={props.closeEvent}>
+                        Close
+                    </button>
+                </header>
+
+                {props.children}
+            </div>
+        </div>
+    );
+};
+
+export default ViewModal;
