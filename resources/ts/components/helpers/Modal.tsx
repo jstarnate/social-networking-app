f38f3bd409613eb7edcc3d9d33462a3d61388a07
Jonathan Tarnate <jstarnate@gmail.com>
2021-01-09 13:13:13 +0800

    Added portal

--- resources/ts/components/helpers/Modal.tsx
+++ resources/ts/components/helpers/Modal.tsx
@@ -1,21 +1,32 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
+import useCloseOnEscape from 'hooks/useCloseOnEscape';
+import useOutsideClick from 'hooks/useOutsideClick';
 
 interface ModalProps {
     title: string;
     type: string;
     message: string | null;
+    closeEvent?: CallableFunction;
     children?: ReactElement;
 }
 
-const AlertBox: FC<ModalProps> = ({
+const Content = ({
     title,
     type,
     message,
+    closeEvent,
     children,
-}: ModalProps): ReactElement => {
+}: ModalProps) => {
+    const modal = useRef<HTMLDivElement>(null);
+
+    useOutsideClick(modal, closeEvent);
+
     return (
         <div className='pos--fixed modal'>
-            <div className='bg--white b-rad--md mg-l--auto mg-r--auto modal__main'>
+            <div
+                ref={modal}
+                className='bg--white b-rad--md mg-l--auto mg-r--auto modal__main'>
                 <header className={`d--flex bg--${type} pd--xs modal__head`}>
                     <h3 className='text--white'>{title}</h3>
                 </header>
@@ -39,4 +50,21 @@ const AlertBox: FC<ModalProps> = ({
     );
 };
 
-export default AlertBox;
+const AlertModal: FC<ModalProps> = (props: ModalProps): ReactElement => {
+    const root = document.querySelector('#portal');
+    const el = document.createElement('section');
+
+    useCloseOnEscape(props.closeEvent);
+
+    useEffect(() => {
+        root?.appendChild(el);
+
+        return () => {
+            root?.removeChild(el);
+        };
+    }, [el, root]);
+
+    return createPortal(<Content {...props} />, el);
+};
+
+export default AlertModal;
