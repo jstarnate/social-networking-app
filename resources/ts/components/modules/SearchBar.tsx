3384e2c49ff014a0265cf20a95b3ac5f0ce884d2
Jonathan Tarnate <jstarnate@gmail.com>
2021-02-14 16:13:00 +0800

    Major changes

--- resources/ts/components/modules/SearchBar.tsx
+++ resources/ts/components/modules/SearchBar.tsx
@@ -1,14 +1,17 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { ChangeEvent, FormEvent, useRef, useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
-import { User } from 'types/models';
 import BasicUserInfo from 'helpers/BasicUserInfo';
 import Ellipsis from 'helpers/Ellipsis';
+import { User } from 'types/models';
+import useOutsideClick from 'hooks/useOutsideClick';
 
 function SearchBar() {
     const [users, setUsers] = useState<User[]>([]);
     const [query, setQuery] = useState<string>('');
+    const [showSuggestions, toggleSuggestions] = useState<boolean>(false);
     const [loading, setLoading] = useState<boolean>(false);
+    const searchComponent = useRef<HTMLElement>(null);
 
     async function getSearchSuggestions(event: ChangeEvent<HTMLInputElement>) {
         const { value } = event.target;
@@ -35,53 +38,62 @@ function SearchBar() {
         }
     }
 
-    return (
-        <form
-            method='GET'
-            action='/users/search'
-            className='d--flex pos--rel'
-            onSubmit={submitForm}>
-            <input
-                className='flex--1 font--md bg--pale text--black pd-t--xs pd-b--xs pd-l--xs search__input'
-                type='text'
-                name='sq'
-                placeholder='Search by name or username'
-                value={query}
-                onChange={getSearchSuggestions}
-            />
+    useOutsideClick(searchComponent, toggleSuggestions.bind(null, false));
 
-            <button className='btn bg--pale pd-l--sm pd-r--sm search__submit'>
-                <i className='fa fa-search font--md text--gray'></i>
-            </button>
+    return (
+        <section ref={searchComponent} className='pos--rel'>
+            <form
+                method='GET'
+                action='/users/search'
+                className='d--flex b--1 brdr--primary b-rad--sm'
+                onSubmit={submitForm}>
+                <input
+                    className='flex--1 text--black pd-t--xs pd-b--xs pd-l--xs search__input'
+                    type='text'
+                    name='sq'
+                    placeholder='Search by name or username'
+                    value={query}
+                    onFocus={toggleSuggestions.bind(null, true)}
+                    onChange={getSearchSuggestions}
+                />
+
+                <button className='btn pd-l--sm pd-r--sm search__submit'>
+                    <i className='fa fa-search text--primary'></i>
+                </button>
+            </form>
 
             {loading && (
-                <section className='pos--abs full-width bg--white b--1 brdr--gray-light search__loading-spinner'>
+                <div className='pos--abs full-width bg--white b--1 brdr--primary search__loading-spinner'>
                     <Ellipsis />
-                </section>
+                </div>
             )}
 
-            {!loading && !!users.length && (
-                <Router>
-                    <section className='pos--abs full-width bg--white b--1 brdr--gray-light search__suggestions'>
-                        {users.map((user: User, i) => (
-                            <BasicUserInfo
-                                key={i}
-                                imageClassName='search__profile-photo'
-                                className='d--flex ai--center pd--xs bt--1 brdr--gray-light search__suggestion'
-                                avatarSize={35}
-                                {...user}
-                            />
-                        ))}
+            {!loading && showSuggestions && !!users.length && (
+                <BrowserRouter>
+                    <div className='pos--abs full-width bg--white b--1 brdr--primary search__suggestions'>
+                        {users.map((user: User, i) => {
+                            const bordered = i ? 'bt--1 brdr--primary' : '';
+
+                            return (
+                                <BasicUserInfo
+                                    key={i}
+                                    imageClassName='search__profile-photo'
+                                    className={`d--flex ai--center pd--xs ${bordered} search__suggestion`}
+                                    avatarSize={35}
+                                    {...user}
+                                />
+                            );
+                        })}
 
                         <a
-                            className='d--block font--sm text--primary-dark text--center bt--1 brdr--gray-light pd-t--xs pd-b--xs'
+                            className='d--block font--sm text--primary-dark text--center bt--1 brdr--primary pd-t--xs pd-b--xs'
                             href={`/users/search?sq=${query}`}>
                             Show all
                         </a>
-                    </section>
-                </Router>
+                    </div>
+                </BrowserRouter>
             )}
-        </form>
+        </section>
     );
 }
 
