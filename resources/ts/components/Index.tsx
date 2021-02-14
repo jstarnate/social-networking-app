8739ac2ef788c3f09337e318a3a13fa9983bc7ff
Jonathan Tarnate <jstarnate@gmail.com>
2021-02-15 00:25:48 +0800

    Added horizontal paddings

--- resources/ts/components/Index.tsx
+++ resources/ts/components/Index.tsx
@@ -28,52 +28,50 @@ function IndexComponent() {
     }
 
     return (
-        <section className='pd-t--lg pd-b--lg mg-l--auto mg-r--auto index__wrap'>
-            <div>
-                <h2 className='text--black-light'>Sign in to your account</h2>
+        <section className='pd-t--sm pd-b--lg pd-l--md pd-r--md mg-l--auto mg-r--auto index__wrap'>
+            <h2 className='text--black-light'>Sign in to your account</h2>
 
-                {!!error && (
-                    <div
-                        data-testid='error-alert'
-                        className='alert--danger b-rad--sm pd--sm mg-t--lg'>
-                        {error}
-                    </div>
-                )}
+            {!!error && (
+                <div
+                    data-testid='error-alert'
+                    className='alert--danger b-rad--sm pd--sm mg-t--sm'>
+                    {error}
+                </div>
+            )}
 
-                <form
-                    data-testid='login-form'
-                    className='mg-t--md'
-                    onSubmit={submit}>
-                    <InputField
-                        id='username'
-                        label='Username'
-                        autoFocus={true}
-                        {...usernameData}
-                    />
+            <form
+                data-testid='login-form'
+                className='mg-t--md'
+                onSubmit={submit}>
+                <InputField
+                    id='username'
+                    label='Username'
+                    autoFocus={true}
+                    {...usernameData}
+                />
 
-                    <InputField
-                        containerClassName='mg-t--md'
-                        id='password'
-                        type='password'
-                        label='Password'
-                        {...passwordData}
-                    />
+                <InputField
+                    containerClassName='mg-t--md'
+                    id='password'
+                    type='password'
+                    label='Password'
+                    {...passwordData}
+                />
 
-                    <div className='d--flex ai--center mg-t--lg'>
-                        <button
-                            data-testid='login-button'
-                            className='btn btn--primary font--md text--bold pd-t--sm pd-b--sm pd-l--lg pd-r--lg b-rad--sm'
-                            disabled={loading}>
-                            Sign in
-                        </button>
-                        <a
-                            className='font--md text--gray mg-l--auto index__forgot-link'
-                            href='/forgot-password'>
-                            I forgot my password
-                        </a>
-                    </div>
-                </form>
-            </div>
+                <div className='d--flex ai--center mg-t--lg'>
+                    <button
+                        data-testid='login-button'
+                        className='btn btn--primary font--md text--bold pd-t--xs pd-b--xs pd-l--lg pd-r--lg b-rad--sm'
+                        disabled={loading}>
+                        Sign in
+                    </button>
+                    <a
+                        className='text--gray mg-l--auto index__forgot-link'
+                        href='/forgot-password'>
+                        I forgot my password
+                    </a>
+                </div>
+            </form>
         </section>
     );
 }
