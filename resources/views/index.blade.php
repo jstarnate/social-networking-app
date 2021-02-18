28f10befc2e277d63a479efab5836fe98a09769c
Jonathan Tarnate <jstarnate@gmail.com>
2021-02-18 16:15:36 +0800

    Made reset password page responsive.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
     <header class="header">
         <div class="pos--rel d--flex mg-l--auto mg-r--auto header__wrap">
             <a class="pos--abs header__logo-link" href="{{ route('index') }}">
-                <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="30" height="30" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="20" cy="20" r="20" fill="black"/>
                     <path d="M6.85718 12H18V29.1429L9.14289 20.2857M33.4286 12H24.8572M24.8572 12H21.7143L9.42861 31.1429M24.8572 12V31.7143" stroke="white" stroke-width="2"/>
                 </svg>
