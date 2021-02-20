661083853eb98de92d1e6a4ea8327444c00785f5
Jonathan Tarnate <jstarnate@gmail.com>
2021-02-20 18:53:08 +0800

    Moved Helmet and BrowserRouter into root.

--- resources/ts/home.tsx
+++ resources/ts/home.tsx
@@ -1,11 +1,19 @@
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import Home from './components/Home';
 import store from './data/store';
 
 render(
     <Provider store={store}>
-        <Home />
+        <Helmet>
+            <title>Home</title>
+        </Helmet>
+
+        <BrowserRouter>
+            <Home />
+        </BrowserRouter>
     </Provider>,
     document.querySelector('#home')
 );
