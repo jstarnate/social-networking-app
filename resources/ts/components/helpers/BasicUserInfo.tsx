001c83825127cbd2f2c2e41fe682697f279b58c4
Jonathan Tarnate <jstarnate@gmail.com>
2021-02-22 04:00:31 +0800

    Added some props and fixed the layout  for smaller devices.

--- resources/ts/components/helpers/BasicUserInfo.tsx
+++ resources/ts/components/helpers/BasicUserInfo.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import { Link } from 'react-router-dom';
 import ProfilePhoto from './ProfilePhoto';
 import { User } from 'types/models';
@@ -6,33 +7,44 @@ interface Props extends User {
     className?: string;
     imageClassName?: string;
     avatarSize?: number;
+    fromSelf?: boolean;
+    children?: ReactNode;
 }
 
 function BasicUserInfo(props: Props) {
     return (
-        <Link className={props.className} to={props.url || ''}>
-            <ProfilePhoto
-                className={props.imageClassName}
-                src={props.image_url}
-                size={props.avatarSize}
-                gender={props.gender}
-                alt='Profile photo'
-            />
+        <div
+            className={props.className}
+            style={{ gridTemplateColumns: 'auto 1fr auto' }}>
+            <Link to={props.url || ''}>
+                <ProfilePhoto
+                    className={props.imageClassName}
+                    src={props.image_url}
+                    size={props.avatarSize}
+                    gender={props.gender}
+                    alt='Profile photo'
+                />
+            </Link>
 
-            <div className='mg-l--xs' style={{ minWidth: '0' }}>
+            <Link
+                className='mg-l--xs'
+                to={props.url || ''}
+                style={{ minWidth: '0' }}>
                 <span className='d--block font--sm text--black-light text--bold truncated'>
                     {props.full_name}
                 </span>
                 <span className='d--block font--sm text--gray truncated'>
                     @{props.username}
                 </span>
-            </div>
-        </Link>
+            </Link>
+
+            {props.children}
+        </div>
     );
 }
 
 BasicUserInfo.defaultProps = {
-    className: 'd--if ai--center home__post-headline',
+    className: 'd--grid ai--center',
 };
 
 export default BasicUserInfo;
