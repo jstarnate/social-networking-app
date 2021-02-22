e014974c5a6d4d7d2a3cf5f96e22cf94c21573c8
Jonathan Tarnate <jstarnate@gmail.com>
2021-02-23 01:29:32 +0800

    Modified

--- resources/ts/components/modules/Sidebar.tsx
+++ resources/ts/components/modules/Sidebar.tsx
@@ -50,8 +50,9 @@ function Sidebar({ user }: Props) {
     function initializeEcho() {
         const echo = new Echo({
             broadcaster: 'pusher',
-            key: process.env.MIX_PUSHER_APP_KEY,
-            cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+            key: '447be77af8401411e761',
+            cluster: 'ap1',
+            encrypted: true,
         });
 
         if (user) {
